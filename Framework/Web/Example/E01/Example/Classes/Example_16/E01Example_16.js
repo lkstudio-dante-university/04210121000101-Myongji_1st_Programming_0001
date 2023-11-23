80144674d5c9e266ab3c404fb8d552b81c3bd6bc
lkstudio.dante <lkstudio.dante@gmail.com>
2023-11-24 03:46:47 +0900

    no message

--- Framework/Web/Example/E01/Example/Classes/Example_16/E01Example_16.js
+++ Framework/Web/Example/E01/Example/Classes/Example_16/E01Example_16.js
@@ -27,14 +27,14 @@ function OnTouchAddMemberBtn() {
 	/*
 	JSON 객체를 활용하면 특정 데이터를 손쉽게 JSON 형식의 문자열로 변환하는 것이 가능하다.
 
-	JSON.parse 함수는 JSON 형식의 문자열을 데이터로 변환하는 역할을 수행하며 JSON.stringify 함수는 데이터를 JSON 형식의 문자열로 변환하는 
-	역할을 수행한다.  (즉, 해당 함수를 활용하면 배열을 비롯한 다양한 자료형의 데이터를 저장 및 로드하는 것이 가능하다.)
+	JSON.parse 함수는 JSON 형식의 문자열을 데이터로 변환하는 역할을 수행하며 JSON.stringify 함수는 데이터를 JSON 형식의 문자열로 
+	변환하는 역할을 수행한다.  (즉, 해당 함수를 활용하면 배열을 비롯한 다양한 자료형의 데이터를 저장 및 로드하는 것이 가능하다.)
 
 	JSON (Java Script Object Notation) 이란?
 	- 문자열 기반의 데이터 포맷을 의미한다. (즉, Java Script 에서 객체를 데이터로 표현하기 위한 형식이라는 것을 알 수 있다.)
 
-	JSON 은 배열을 비롯한 여러 데이터를 표현하는 것이 가능하기 때문에 현재 현업에서도 많이 활용되는 데이터 포맷이다. (즉, JSON 이외에도 CSV 와
-	같은 데이터 포맷이 존재한다는 것을 알 수 있다.)
+	JSON 은 배열을 비롯한 여러 데이터를 표현하는 것이 가능하기 때문에 현재 현업에서도 많이 활용되는 데이터 포맷이다. (즉, JSON 이외에도 
+	CSV 와 같은 데이터 포맷이 존재한다는 것을 알 수 있다.)
 	*/
 	var oMemberList = JSON.parse(oJSONStr);
 	oMemberList.push(oMember);
@@ -48,8 +48,8 @@ function OnTouchAddMemberBtn() {
 /** 모든 회원 보기 버튼을 눌렀을 경우 */
 function OnTouchPrintAllMembersBtn() {
 	/*
-	window 객체의 location 속성을 활용하면 다른 웹 문서를 로드하는 것이 가능하다. (즉, 해당 속성은 HTML 의 a 태그와 유사하다는 것을 알 수 
-	있다.)
+	window 객체의 location 속성을 활용하면 다른 웹 문서를 로드하는 것이 가능하다. (즉, 해당 속성은 HTML 의 a 태그와 유사하다는 것을 
+	알 수 있다.)
 	*/
 	window.location.href = "./E01Members_16.html";
 }
